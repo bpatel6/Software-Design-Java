0fdaec86b13ff803ec75f4274874b625fd304371
Bhavik Patel <bpatel6@uiowa.edu>
2020-02-18 08:04:46 -0600

    hw 1 almost done

--- oral_exam1/S3_Barcode/src/TEST.java
+++ oral_exam1/S3_Barcode/src/TEST.java
@@ -11,8 +11,15 @@ public class TEST {
         System.out.println(post.getZipcode());
         System.out.println(post.ziptobinary());
 
-        UPC a = new UPC("03600029145");
+        UPC a = new UPC("01254667375");
         System.out.println(a.checksum());
         System.out.println(a.getupc());
+        a.encrypt();
+       // System.out.println(a.encrypt());
+        System.out.println(a.decrypt(a.getEncryptedupc()));
+        UPC b = new UPC("01231564313");
+        b.checksum();
+        System.out.println(b.encrypt());
+        System.out.println(b.decrypt(b.getEncryptedupc()));
     }
 }
--- oral_exam1/S3_Barcode/src/UPC.java
+++ oral_exam1/S3_Barcode/src/UPC.java
@@ -1,6 +1,6 @@
 public class UPC {
     private String upc;
-
+    private String encryptedupc;
     private int checkdigit;
     public String getupc() {
         return upc;
@@ -11,7 +11,13 @@ public class UPC {
     public int getCheckdigit() {
         return checkdigit;
     }
+    public String getEncryptedupc() {
+        return encryptedupc;
+    }
 
+    public void setEncryptedupc(String encryptedupc) {
+        this.encryptedupc = encryptedupc;
+    }
     public void setCheckdigit(int checkdigit) {
         this.checkdigit = checkdigit;
     }
@@ -31,10 +37,7 @@ public class UPC {
                 sumeven += Integer.parseInt(String.valueOf(upc.charAt(i)));
             }
         }
-        System.out.println(sumeven);
-        System.out.println(sumodd);
         finalsum = (sumodd*3) + sumeven;
-        System.out.println(finalsum);
         if (finalsum%10 == 0){
             checkdigit = 0;
         }
@@ -43,5 +46,136 @@ public class UPC {
         }
         upc = upc + checkdigit;
         return checkdigit;
+    }
+
+    public String encrypt(){
+        StringBuilder buildencrypt = new StringBuilder();
+        buildencrypt.append("101 ");
+        buildencrypt.append(encryptleft(upc.substring(0,6)));
+        buildencrypt.append(" 01010 ");
+        buildencrypt.append(encryptright(upc.substring(6,12)));
+        buildencrypt.append(" 101");
+        this.encryptedupc = String.valueOf(buildencrypt);
+        return encryptedupc;
+    }
+
+    public String decrypt(String input){
+        StringBuilder build = new StringBuilder();
+        String left = input.substring(4,46);
+        String right = input.substring(53,95);
+        build.append(decrypthelper(left));
+        build.append(decrypthelper(input.substring(53,95)));
+        return String.valueOf(build);
+    }
+
+    public StringBuilder encryptleft(String input){
+        StringBuilder binary = new StringBuilder();
+        for(int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == '0'){
+                binary.append("0001101");
+            }
+            else if (input.charAt(i) == '1') {
+                binary.append("0011001");
+            }
+            else if (input.charAt(i) == '2') {
+                binary.append("0010011");
+            }
+            else if (input.charAt(i) == '3') {
+                binary.append("0111101");
+            }
+            else if (input.charAt(i) == '4') {
+                binary.append("0100011");
+            }
+            else if (input.charAt(i) == '5') {
+                binary.append("0110001");
+            }
+            else if (input.charAt(i) == '6') {
+                binary.append("0101111");
+            }
+            else if (input.charAt(i) == '7') {
+                binary.append("0111011");
+            }
+            else if (input.charAt(i) == '8') {
+                binary.append("0110111");
+            }
+            else if (input.charAt(i) == '9') {
+                binary.append("0001011");
+            }
+        }
+        return binary;
+    }
+
+    public StringBuilder encryptright(String input){
+        StringBuilder binary = new StringBuilder();
+        for(int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == '0'){
+                binary.append("1110010");
+            }
+            else if (input.charAt(i) == '1') {
+                binary.append("1100110");
+            }
+            else if (input.charAt(i) == '2') {
+                binary.append("1101100");
+            }
+            else if (input.charAt(i) == '3') {
+                binary.append("1000010");
+            }
+            else if (input.charAt(i) == '4') {
+                binary.append("1011100");
+            }
+            else if (input.charAt(i) == '5') {
+                binary.append("1001110");
+            }
+            else if (input.charAt(i) == '6') {
+                binary.append("1010000");
+            }
+            else if (input.charAt(i) == '7') {
+                binary.append("1000100");
+            }
+            else if (input.charAt(i) == '8') {
+                binary.append("1001000");
+            }
+            else if (input.charAt(i) == '9') {
+                binary.append("1110100");
+            }
+        }
+        return binary;
+    }
+
+    public String decrypthelper(String input){
+        StringBuilder upc = new StringBuilder();
+        for (int i = 0; i < input.length(); i = i+7){
+            if (input.substring(i, i + 7).equals("0001101") || input.substring(i, i + 7).equals("1110010")){
+                upc.append("0");
+            }
+            else if (input.substring(i, i + 7).equals("0011001") || input.substring(i, i + 7).equals("1100110")){
+                upc.append("1");
+            }
+            else if (input.substring(i, i + 7).equals("0010011") || input.substring(i, i + 7).equals("1101100")){
+                upc.append("2");
+            }
+            else if (input.substring(i, i + 7).equals("0111101") || input.substring(i, i + 7).equals("1000010")){
+                upc.append("3");
+            }
+            else if (input.substring(i, i + 7).equals("0100011") || input.substring(i, i + 7).equals("1011100")){
+                upc.append("4");
+            }
+            else if (input.substring(i, i + 7).equals("0110001") || input.substring(i, i + 7).equals("1001110")){
+                upc.append("5");
+            }
+            else if (input.substring(i, i + 7).equals("0101111") || input.substring(i, i + 7).equals("1010000")){
+                upc.append("6");
+            }
+            else if (input.substring(i, i + 7).equals("0111011") || input.substring(i, i + 7).equals("1000100")){
+                upc.append("7");
+            }
+            else if (input.substring(i, i + 7).equals("0110111") || input.substring(i, i + 7).equals("1001000")){
+                upc.append("8");
+            }
+            else if (input.substring(i, i + 7).equals("0001011") || input.substring(i, i + 7).equals("1110100")) {
+                upc.append("9");
+            }
+        }
+        return String.valueOf(upc);
     }
 }
