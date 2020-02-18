5b539925e9f1c4460954cb45037efde51d1cd6e8
Bhavik Patel <bpatel6@uiowa.edu>
2020-02-18 09:17:43 -0600

    quiz 3

---
+++ quizzes/20200217-Quiz/src/StudentJUnitTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentJUnitTest {
+    private Student student = new Student("",0);
+
+    @BeforeEach
+    public void setUpStudent() {
+
+    }
+
+    @Test
+    public void testSetName(){
+        student.setName("Thomas");
+        assertEquals("Thomas",student.getName(),"Thomas");
+    }
+
+    @Test
+    public void setAverageZero(){
+        student.setAverage(0);
+        assertEquals(0,student.getAverage(),"Average is zero!");
+    }
+
+}
