3b93bf63eee45372636de796c99b3655b57bfe90
Bhavik Patel <bpatel6@uiowa.edu>
2020-03-06 15:46:06 -0600

    updates

--- oral_exam1/S23_RequestInheritance_Easy/src/DataGenerator.java
+++ oral_exam1/S23_RequestInheritance_Easy/src/DataGenerator.java
@@ -9,15 +9,40 @@ import java.util.UUID;
  */
 class DataGenerator {
     // fields to hold example data
+    /**
+     * rand - stores random integer generated
+     */
     private Random rand;
+    /**
+     * names - stores names
+     */
     private String[] names;
+    /**
+     * encryption - stores encryption schemes
+     */
     private String[] encryption;
+    /**
+     * videoTitles - stores video Title
+     */
     private String[] videoTitles;
+    /**
+     * filePaths - stores file path
+     */
     private String[] filePaths;
+    /**
+     * fileTypes - stores file type
+     */
     private String[] fileTypes;
+    /**
+     * colors - stores colors
+     */
     private String[] colors;
 
     // Constructor
+
+    /**
+     * Default constructor initialize rand, names, encryption, videoTitles, filePaths, fileTypes, colors
+     */
     DataGenerator() {
         // initialize with example data
         this.rand = new Random();
@@ -30,51 +55,100 @@ class DataGenerator {
     }
 
     // private utilities for data generation
+
+    /**
+     * Selects the random name from the names[]
+     * @return String - random name from the names[]
+     */
     private String getRandName() {
         return this.names[this.rand.nextInt(this.names.length)];
     }
 
+    /**
+     * Selects the random color from the colors[]
+     * @return String - random color from the colors[]
+     */
     private String getRandColor() {
         return this.colors[this.rand.nextInt(this.colors.length)];
     }
 
+    /**
+     * Selects the random video title from the videoTitles[]
+     * @return String - random video title from the videoTitles[]
+     */
     private String getRandVideoTitle() {
         return this.videoTitles[this.rand.nextInt(this.videoTitles.length)];
     }
 
+    /**
+     * Selects the random filetype from the fileTypes[]
+     * @return String - random filetype from the fileTypes[]
+     */
     private String getRandFileType() {
         return this.fileTypes[this.rand.nextInt(this.fileTypes.length)];
     }
 
+    /**
+     * Selects the random filepath from the filePaths[]
+     * @return String - random filepath from the filePaths[]
+     */
     private String getRandFilePath() {
         return this.filePaths[this.rand.nextInt(this.filePaths.length)];
     }
 
     // public data generation tools
+    /**
+     * Generates random URL
+     * @return String - random URL
+     */
     public String getRandURL() {
         return "localhost:" + (((this.rand.nextInt(9) + 1) * 1000) + this.rand.nextInt(999));
     }
 
+    /**
+     * Generates random payment
+     * @return String - random payment
+     */
     public Payment getRandPayment() {
         return new Payment(this.getRandName(), this.rand.nextInt(10000), this.getRandName());
     }
 
+    /**
+     * Generates random UUID
+     * @return String - random UUID
+     */
     public UUID getRandUUID() {
         return UUID.randomUUID();
     }
 
+    /**
+     * Selects the random EncryptionScheme from the encryption[]
+     * @return String - random EncryptionScheme from the encryption[]
+     */
     public String getRandEncryptionScheme() {
         return this.encryption[this.rand.nextInt(this.encryption.length)];
     }
 
+    /**
+     * Generates the random Video
+     * @return Video - random Video
+     */
     public Video getRandVideo() {
         return new Video(this.getRandURL(), this.getRandVideoTitle(), this.getRandName());
     }
 
+    /**
+     * Generates the random file
+     * @return File - random file
+     */
     public File getRandFile() {
         return new File(this.getRandFilePath(), this.getRandFileType());
     }
 
+    /**
+     * Generates the random form
+     * @return Form - random form
+     */
     public Form getRandForm() {
         HashMap<String, String> fields = new HashMap<>();
         fields.put("Favorite color", this.getRandColor());
@@ -83,7 +157,10 @@ class DataGenerator {
         return new Form(fields);
     }
 
-
+    /**
+     * Generates random IP
+     * @return String - random IP
+     */
     public String getRandIP() {
         return rand.nextInt(256) + "." + rand.nextInt(256) + "." + rand.nextInt(256) + "." + rand.nextInt(256);
     }
